98994d648098c4351c4b83370fd874d0137d07a0
pnadjafov <pnadjafov@bayviewglen.ca>
2014-09-22 15:41:16 -0400

    Implemented basic Contact class.

--- Unit 2/src/com/bayviewglen/contactProject/Contact.java
+++ Unit 2/src/com/bayviewglen/contactProject/Contact.java
@@ -1,5 +1,36 @@
 package com.bayviewglen.contactProject;
 
 public class Contact {
-
+	private String firstName;
+	private String lastName;
+	private String phone;
+	
+	public Contact(String fName, String lName, String phoneNum){
+		firstName = fName;
+		lastName = lName;
+		phone = phoneNum;
+	}
+	
+	public String getFirstName(){
+		return firstName;
+	}
+	
+	public void changefirstName(String input){
+		firstName = input;
+	}
+	
+	public String getLastName(){
+		return lastName;
+	}
+	
+	public void changeLastName(String input){
+		lastName = input;
+	}
+	public String getPhone(){
+		return phone;
+	}
+	
+	public void changePhone(String input){
+		phone = input;
+	}
 }
