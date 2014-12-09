d556e7a1cab729f0bf5a1e1e29068fb755f2b9d7
pnadjafov <pnadjafov@bayviewglen.ca>
2014-12-08 20:26:54 -0500

    Added comments and constants for both classes. Got rid of the final bugs from BadNeighbors, which required me to re-think my approach on an important portion of the code. Both classes should work for all problems now.

--- Dynamic Programming Assignment/src/com/bayviewglen/questions/BadNeighbors.java
+++ Dynamic Programming Assignment/src/com/bayviewglen/questions/BadNeighbors.java
@@ -1,49 +1,39 @@
 package com.bayviewglen.questions;
 
 public class BadNeighbors {
-	public static void main(String[] args) {
+	
+	public static void main(String[] args){
 		int[] neighbors1 = { 94, 40, 49, 65, 21, 21, 106, 80, 92, 81, 679, 4, 61,  
-				  6, 237, 12, 72, 74, 29, 95, 265, 35, 47, 1, 61, 397,
-				  52, 72, 37, 51, 1, 81, 45, 435, 7, 36, 57, 86, 81, 72 };
+				6, 237, 12, 72, 74, 29, 95, 265, 35, 47, 1, 61, 397,
+				52, 72, 37, 51, 1, 81, 45, 435, 7, 36, 57, 86, 81, 72 };
 		System.out.println("Maximum possible funds to save the well: $" + doorToDoor(neighbors1) + ".");
-		
-		int[] neighbors2 = { 7, 7, 7, 7, 7, 7, 7};
+
+		int[] neighbors2 = { 1, 2, 3, 4, 5, 1, 2, 3, 4, 5 };
 		System.out.println("Maximum possible funds to save the well: $" + doorToDoor(neighbors2) + ".");
 	}
 
 	public static int doorToDoor(int[] neighbors){
-		if(neighbors.length == 0){
+		if(neighbors.length == 0){ // If the array is empty, simply return 0.
 			return 0;
-		}else if(neighbors.length == 1){
+		}else if(neighbors.length == 1){ // If the array has one value, return the first value.
 			return neighbors[0];
-		}else if(neighbors.length == 2){
+		}else if(neighbors.length == 2){ // If the array has two values, return the greater of the two values.
 			return Math.max(neighbors[0], neighbors[1]);
-		}else if(neighbors.length == 3){
+		}else if(neighbors.length == 3){ // If the array has three values, return the greater value of the three values.
 			return Math.max(Math.max(neighbors[0], neighbors[1]), neighbors[2]);
 		}else{
-			int[] funds = new int[neighbors.length];
-			boolean firstIndex = false;
-			funds[0] = neighbors[0];
-			funds[1] = Math.max(neighbors[0], neighbors[1]);
-			if(funds[1] == neighbors[0]){
-				firstIndex = true;
-			}else if(funds[0] + funds[2] > funds[1]){
-				firstIndex = true;
+			int[] solutionsOne = new int[neighbors.length]; 
+			int[] solutionsTwo = new int[neighbors.length];
+			solutionsOne[0] = neighbors[0]; // Solve when considering the first value and not the last one.
+			for (int i = 2; i < neighbors.length - 1; i++){ 
+			solutionsOne[i] = Math.max(solutionsOne[i - 2] + neighbors[i], solutionsOne[i - 1]); // Checks if the solution to the number before
+			} // the current index is bigger than the solution to the neighbor 2 elements before it, 
+			  //including its own value. Whichever solution is bigger will be stored in the solutionsOne array.
+			solutionsTwo[1] = neighbors[1]; // Solve when considering the last value and not the first one.
+			for (int i = 2; i < neighbors.length; i++){
+			solutionsTwo[i] = Math.max(solutionsTwo[i - 2] + neighbors[i], solutionsTwo[i - 1]); // Same statement as above, but the greater solution is stored in the solutionsTwo array.
 			}
-			for(int i = 2; i<neighbors.length; i++){
-				if(i == neighbors.length-1 && firstIndex){
-					if(funds[i-2] - neighbors[0] + neighbors[i] > funds[i-2]){
-						funds[i] = funds[i-2] - neighbors[0] + neighbors[i];
-					}else{
-						funds[i] = funds[i-1];
-					}
-				}else if(funds[i-2] + neighbors[i] > funds[i-1]){
-					funds[i] = funds[i-2] + neighbors[i];
-				}else{
-					funds[i] = funds[i-1];
-				}
+			return Math.max(solutionsOne[solutionsOne.length - 2], solutionsTwo[solutionsTwo.length - 1]); // Return the greater value between the two solution arrays.
 			}
-			return funds[funds.length-1];
 		}
-	}
 }
--- Dynamic Programming Assignment/src/com/bayviewglen/questions/ZigZag.java
+++ Dynamic Programming Assignment/src/com/bayviewglen/questions/ZigZag.java
@@ -2,7 +2,12 @@ package com.bayviewglen.questions;
 
 public class ZigZag {
 
+	private static final int EMPTY = 0;
+	private static final int MAX = 1;
+	private static final int FIRST = 1;
+
 	public static void main(String[] args) {
+		
 		int[] problem = { 374, 40, 854, 203, 203, 156, 362, 279, 812, 955, 
 				600, 947, 978, 46, 100, 953, 670, 862, 568, 188, 
 				67, 669, 810, 704, 52, 861, 49, 640, 370, 908, 
@@ -19,28 +24,28 @@ public class ZigZag {
 	}
 	
 	public static int zigZag(int[] sequence){
-		if(sequence.length == 0){
-			return 0;
-		}else if(sequence.length == 1){
-			return 1;
+		if(sequence.length == EMPTY){ // If the sequence is empty, return 0.
+			return EMPTY;
+		}else if(sequence.length == FIRST){ // If the sequence has only one value, return the only value in the sequence.
+			return FIRST;
 		}else{
-			int[] difference = new int[sequence.length];
-			int maximum = 1;
+			int[] difference = new int[sequence.length]; // Stores all the calculated differences.
+			int maximum = MAX;
 			boolean maxLimit;
-			for(int i = 0; i<sequence.length-1; i++){
+			for(int i = 0; i<sequence.length-1; i++){ // A for loop that stores all the differences into the difference array.
 				difference[i] = sequence[i+1] - sequence[i];
-			}if(difference[0] >= 0){
+			}if(difference[0] >= EMPTY){ // Checks if the first value in the difference array has a value greater than or equal to 0.
 				maxLimit = true;
 			}else{
 				maxLimit = false;
 			}
 			for(int k = 1; k<sequence.length; k++){
 				boolean keepGoing = maxLimit;
-				if(difference[k] > 0){
+				if(difference[k] > 0){ // Checks if the difference is positive.
 					keepGoing = true;
-				}else if(difference[k] < 0){
+				}else if(difference[k] < 0){ // Checks if the difference is negative.
 					keepGoing = false;
-				}if(keepGoing != maxLimit){
+				}if(keepGoing != maxLimit){ // If these two booleans disagree, increase the value of the maximum integer, and keep going.
 					maximum++;
 					maxLimit = keepGoing;
 				}
