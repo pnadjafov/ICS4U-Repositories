a30877728afc3a4510b42568110e59faa9b0cb45
pnadjafov <pnadjafov@bayviewglen.ca>
2015-06-12 12:52:27 -0400

    First push of the game "Pop". Two completed levels, Level One: Circle and Level Two: Square; still need to finish Level Three: Triangle. Need to finish the last level, link the levels together, add in a few more levels to increase the play time, add a pause button, add pause UI, add the game's main menu and also the loading screen for when the app launches. In the final touches we should add comments too.

---
+++ PopTheGame/ISP Game/js/pop7.js
@@ -0,0 +1,91 @@
+var radius2 = 0;
+var canvas = document.getElementById('game');
+var clicks = 0;
+var win = false;
+
+$("#game").click(function(e) {
+    clicks++;
+    if (clicks == 1) {
+        grow();
+    } else if (clicks == 2) {
+        if (radius2 >= 110 && radius2 < 125) {
+            win = true;
+        }
+    }
+
+});
+
+
+$(document).ready(function() {
+    var canvas = document.getElementById('game');
+    var ctx = canvas.getContext("2d");
+    var centerX = canvas.width / 2;
+    var centerY = canvas.height / 2;
+    var radius = 125;
+
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = 'white';
+    ctx.fill();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = '#003300';
+    ctx.stroke();
+
+    var radius2 = 0;
+    var centerX2 = canvas.width / 2;
+    var centerY2 = canvas.height / 2;
+    var ctx2 = canvas.getContext("2d");
+    ctx2.beginPath();
+    ctx2.arc(centerX2, centerY2, radius2, 0, 2 * Math.PI, false);
+    ctx2.fillStyle = "#ed1c24";
+    ctx2.fill();
+    ctx2.lineWidth = 0;
+
+
+
+    var ctx3 = canvas.getContext("2d");
+
+    ctx3.font = "50px Jelly";
+    ctx3.fillText("Level 6", 10, 50);
+})
+
+
+function grow() {
+    if (clicks == 2) {
+        if (radius2 >= 125 && radius2 <= 130) {
+            var winMessage = canvas.getContext("2d");
+            winMessage.font = "50px Jelly";
+            winMessage.fillStyle = 'black';
+            winMessage.fillText("You Win!", 60, 265);
+            return;
+        } else {
+            var loseMessage = canvas.getContext("2d");
+            loseMessage.font = "50px Jelly";
+            loseMessage.fillStyle = 'black';
+            loseMessage.fillText("You Lose!", 60, 265);
+            return;
+        }
+        clicks = 0;
+        win = false;
+        return;
+    } else {
+        radius2 = radius2 + 4;
+        var centerX2 = canvas.width / 2;
+        var centerY2 = canvas.height / 2;
+        var ctx2 = canvas.getContext("2d");
+        ctx2.beginPath();
+        ctx2.arc(centerX2, centerY2, radius2, 0, 2 * Math.PI, false);
+        ctx2.fillStyle = '#ed1c24';
+        ctx2.fill();
+        ctx2.lineWidth = 0;
+        if (radius2 > 135) {
+            var loseMessage = canvas.getContext("2d");
+            loseMessage.font = "50px Jelly";
+            loseMessage.fillStyle = 'black';
+            loseMessage.fillText("You Lose!", 60, 265);
+            return;
+        }
+
+    }
+    setTimeout(grow, 20);
+}
